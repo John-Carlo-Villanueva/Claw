5e1cb97eeead8eaf6dabdf0b182633632a186140
John-Carlo-Villanueva <johncarlovillanueva2008@gmail.com>
2023-02-03 15:16:51 -1000

    declared button

--- robotCode/src/main/java/frc/robot/RobotContainer.java
+++ robotCode/src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import frc.robot.subsystems.ClawSubsystem;
 import frc.robot.commands.ClawClampCmd;
 import frc.robot.commands.ClawReleaseCmd;
@@ -11,10 +12,12 @@ import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
   private final ClawSubsystem clawSubsystem = new ClawSubsystem();
+  private final Joystick stickA = new Joystick(0);
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   private final CommandXboxController m_driverController =
@@ -25,6 +28,11 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
+
+    new JoystickButton(stickA, 1).onTrue(new ClawClampCmd(clawSubsystem));
+    new JoystickButton(stickA, 4).onTrue(new ClawReleaseCmd(clawSubsystem));
+    new JoystickButton(stickA, 3).onTrue(new ClawTCCWCmd(clawSubsystem));
+    new JoystickButton(stickA, 2).onTrue(new ClawTCWCmd(clawSubsystem));
     new Trigger(m_exampleSubsystem::exampleCondition)
         .onTrue(new ExampleCommand(m_exampleSubsystem));
     m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
