a4036894e5c310cb388f4df136b5a1a0458ca090
John-Carlo-Villanueva <johncarlovillanueva2008@gmail.com>
2023-02-11 11:02:29 -1000

    deleted files I don't use

--- robotCode/src/main/java/frc/robot/RobotContainer.java
+++ robotCode/src/main/java/frc/robot/RobotContainer.java
@@ -8,23 +8,13 @@ import frc.robot.commands.ClawMidCmd;
 import frc.robot.commands.ClawReleaseCmd;
 import frc.robot.commands.ClawTCCWCmd;
 import frc.robot.commands.ClawTCWCmd;
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
   private final ClawSubsystem clawSubsystem = new ClawSubsystem();
   private final Joystick stickA = new Joystick(0);
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
   public RobotContainer() {
     configureBindings();
@@ -39,13 +29,9 @@ public class RobotContainer {
     new JoystickButton(stickA, 5).onTrue(new ClawMidCmd(clawSubsystem)); // Button for default position of Claw
     new JoystickButton(stickA, 6).onTrue(new InstantCommand(()-> clawSubsystem.resetWristEnc())); // Button to reset Enc
     new JoystickButton(stickA, 7).toggleOnTrue(new ClawCmd(clawSubsystem,() ->stickA.getZ())); // Button that toggle manual encoders
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return null;
   }
 }
